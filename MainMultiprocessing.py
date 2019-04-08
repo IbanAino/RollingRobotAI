d7a10bb8d45ac8ba278c081f677197bf122e1ea9
ibangabo <iban.fernandez@laposte.net>
2019-04-08 15:54:24 +0200

    Main process with Lidar works.

--- MainMultiprocessing.py
+++ MainMultiprocessing.py
@@ -5,6 +5,7 @@ from Modules.Robot import Robot
 import matplotlib.pyplot as plt
 import time
 import threading
+import multiprocessing
 
 
 show_animation = True
@@ -12,7 +13,7 @@ show_animation = True
 
 OccupancyGrid = OccupancyGrid(200, 200, 50) # (gridHigh, gridWidth, cellSize)
 grid = OccupancyGrid.grid
-print(grid)
+#print(grid)
 
 Robot = Robot()
 Robot.x = 5000
@@ -61,20 +62,37 @@ def UpdateOccupancyGrid():
         OccupancyGrid.RaycastFillsCells(Robot.x, Robot.y, i[0], i[1])            
         OccupancyGrid.FillCell(i[0], i[1], 255)
     
-def PlotFigure():
+def PlotFigure(name):
     '''
-    plt.imshow(grid) 
-    plt.draw()
-    plt.pause(0.1)
+    while q.empty() is False:       
+        plt.imshow(mainMap) 
+        plt.draw()
+        plt.pause(0.1)
     '''
+    print(name)
+    print("------------------------------------")
+
+    
            
 # SETUP
 if __name__ == '__main__':
     
+    # queue to share variables between process
+    # mainMap = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+    # q = multiprocessing.Queue()
+    
+    # Process Declarations
+    #p1 = multiprocessing.Process(target=PlotFigure, args=(mainMap, q))
+    print("Create process")
+    pr1 = multiprocessing.Process(target=PlotFigure, args=('bob',))
+    pr1.start()
+    pr1.join()
+    print("End process")
+    
     # Threads declataitons
     t1 = threading.Thread(name='daemon', target = GetLidarData)
     t2 = threading.Thread(name='daemon', target = UpdateOccupancyGrid)
-    t3 = threading.Thread(name='daemon', target = PlotFigure)
+    # t3 = threading.Thread(name='daemon', target = PlotFigure)
 
     while True:
         newTime = time.time()
@@ -87,25 +105,28 @@ if __name__ == '__main__':
             t2 = threading.Thread(name='daemon', target = UpdateOccupancyGrid)
             t2.start()
             #t2.join(5)
+        '''
         if t3.isAlive() is False:
             t3 = threading.Thread(name='daemon', target = PlotFigure)
             t3.start()
             #t2.join(5)
-            
+        '''    
          
         # Waiting for Threads to be completed
         t1.join()
         t2.join() # Wait 5 seconds until the thread exit
-        t3.join()
-         
-         
-
+        # t3.join()
         
+        #p1.join()
+
+
+        '''
         if show_animation:
             plt.imshow(grid)
             plt.draw()
             plt.pause(0.1)
-            
+        '''
+        
         # Time for the loop to execute in seconds
         print(time.time() - newTime)
         
---
+++ MainParallellTasks.py
@@ -0,0 +1,111 @@
+import multiprocessing
+import time
+import threading
+import numpy as np
+from Modules.OccupancyGrid import OccupancyGrid
+from Modules.RpLidar import RpLidar
+from Modules.Robot import Robot
+import matplotlib.pyplot as plt
+
+
+#--------------------- FUNCTIONS ---------------
+
+def f(name):
+    print ('hello', name)
+
+
+#------------------------ PROCESS ----------------
+  
+def ProcessRobotManagement():
+    #------ Objects declarations -------
+    global OccupancyGrid
+    global Robot
+    global Lidar
+    
+    #------ Objects instanciations ------
+    occupancyGrid = OccupancyGrid(200, 200, 50) # (gridHigh, gridWidth, cellSize)
+    Robot = Robot()
+    Robot.x = 5000
+    Robot.y = 5000
+    
+    lidar = RpLidar()
+    lidar.runLidar()
+    
+    #------- Variables --------
+    show_animation = True
+    grid = occupancyGrid.grid
+    newTime = 0
+    timer = time.time()
+    
+    #------- FUNCTIONS -------------
+    def GetLidarData():
+        print("GetLidarData thread")
+        global obstaclesFromRobot
+        obstaclesFromRobot = lidar.GetObstacles()
+        #print(obstaclesFromRobot)
+        #print("-----")
+    
+    def UpdateOccupancyGrid():
+        print("OccupancyGrid thread")
+        # print(obstaclesFromRobot)
+        
+        # Translate obstacles positions from robot to world
+        obstaclesFromWorld = np.array([[80, 70]])
+            
+        # passage matrix
+        TrobotToWorld = np.array([[np.cos(Robot.angle), (np.sin(Robot.angle))*-1, Robot.x],
+            [np.sin(Robot.angle), np.cos(Robot.angle), Robot.y],
+            [0, 0, 1]])
+        
+        # Try here if obstaclesFromRobot is defined
+        
+        for obs in obstaclesFromRobot:
+            # Translate the point from the robot to the world  
+            pointFromRobot = np.array([[obs.item(0)], [obs.item(1)], [1]])
+            pointFromWorld = np.dot(TrobotToWorld, pointFromRobot)
+            # Add the new point to the list
+            obstaclesFromWorld = np.append(obstaclesFromWorld, [[pointFromWorld[0, 0], pointFromWorld[1, 0]]], axis=0) 
+        
+        for i in obstaclesFromWorld:
+            occupancyGrid.RaycastFillsCells(Robot.x, Robot.y, i[0], i[1])            
+            occupancyGrid.FillCell(i[0], i[1], 255)
+            #print(i)
+    
+    
+    #------ Threads declarations -------
+    t1 = threading.Thread(name='daemon', target = GetLidarData)
+    t2 = threading.Thread(name='daemon', target = UpdateOccupancyGrid)
+    
+    
+    #--------- MAIN LOOP -----------
+    while True:
+        if t1.isAlive() is False:
+            t1 = threading.Thread(name='daemon', target = GetLidarData)
+            t1.start()
+            #t1.join()
+        if t2.isAlive() is False:
+            t2 = threading.Thread(name='daemon', target = UpdateOccupancyGrid)
+            t2.start()
+
+        t1.join()
+        t2.join()
+        
+        
+        plt.imshow(grid)
+        plt.draw()
+        plt.pause(0.1)
+        
+        
+        #print(grid)
+        
+        
+
+#--------------------- SETUP ---------------
+
+if __name__ == '__main__':
+    p = multiprocessing.Process(target=f, args=('bob',))
+    p2 = multiprocessing.Process(target=ProcessRobotManagement, )
+    p.start()
+    p2.start()
+    p2.join()
+    p.join()
